e88da3345194959d0bed6c4ac90b71a553396acd
hezj6n <hamza.el.aasemy@scania.com>
2024-12-03 13:57:43 +0100

    fixing nav issue

--- components/ui/nav.tsx
+++ components/ui/nav.tsx
@@ -1,51 +1,60 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { defineCustomElements, TdsHeader, TdsHeaderBrandSymbol, TdsHeaderHamburger, TdsHeaderTitle } from '@scania/tegel-react';
+import {
+	defineCustomElements,
+	TdsHeader,
+	TdsHeaderBrandSymbol,
+	TdsHeaderHamburger,
+	TdsHeaderTitle,
+} from '@scania/tegel-react';
 import { Button } from './button';
 import { signOutAction } from '@/app/actions';
 import { createClient } from '@/utils/supabase/client';
+import clsx from 'clsx';
 
-interface NavProps {
-  isLogged: boolean;
-}
+const Nav = () => {
+	const [session, setSession] = useState<any>(null);
+	const supabase = createClient();
 
-const Nav = ({ isLogged }: NavProps) => {
-  const [session, setSession] = useState<any>(null); 
-  const supabase = createClient();
+	useEffect(() => {
+		const fetchSession = async () => {
+			const {
+				data: { session },
+			} = await supabase.auth.getSession();
+			setSession(session);
+		};
+		fetchSession();
+	}, [supabase]);
 
-  useEffect(() => {
-    const fetchSession = async () => {
-      const { data: { session } } = await supabase.auth.getSession();
-      setSession(session); 
-    };
-    fetchSession();
-  }, [supabase]);
+	defineCustomElements();
 
-  defineCustomElements();
+	return (
+		<TdsHeader>
+			<TdsHeaderHamburger
+				onClick={() => {}}
+				aria-label="Open application drawer"
+				aria-haspopup="true"
+				aria-expanded="false"
+			/>
+			<TdsHeaderTitle>Github Dashboard</TdsHeaderTitle>
 
-  return (
-    <TdsHeader>
-      <TdsHeaderHamburger
-        onClick={() => {}}
-        aria-label="Open application drawer"
-        aria-haspopup="true"
-        aria-expanded="false"
-      />
-      <TdsHeaderTitle>Github Dashboard</TdsHeaderTitle>
+			<Button
+				type="submit"
+				variant={'outline'}
+				onClick={signOutAction}
+				className={clsx({
+					invisible: !session?.user,
+				})}
+			>
+				Sign out
+			</Button>
 
-      {session?.user && (
-        <form action={signOutAction}>
-          <Button type="submit" variant={'outline'}>
-            Sign out
-          </Button>
-        </form>
-      )}
-      <TdsHeaderBrandSymbol slot="end">
-        <a aria-label="Scania - red gryphon on blue shield"></a>
-      </TdsHeaderBrandSymbol>
-    </TdsHeader>
-  );
+			<TdsHeaderBrandSymbol slot="end">
+				<a aria-label="Scania - red gryphon on blue shield"></a>
+			</TdsHeaderBrandSymbol>
+		</TdsHeader>
+	);
 };
 
 export { Nav };
