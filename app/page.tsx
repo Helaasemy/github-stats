122ceaa1bbdaad432767c9495141da82132010ab
hezj6n <hamza.el.aasemy@scania.com>
2024-12-03 11:54:40 +0100

    fix logout button in nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,4 @@
-import { signInAction } from "@/app/actions";
-import { FormMessage, Message } from "@/components/form-message";
-import { SubmitButton } from "@/components/submit-button";
-import Link from "next/link";
-
-export default async function Login(props: { searchParams: Promise<Message> }) {
-  const searchParams = await props.searchParams;
+export default async function Login() {
   return (
    <div>
     Welcome to Github Stats
--- components/ui/nav.tsx
+++ components/ui/nav.tsx
@@ -1,44 +1,51 @@
 'use client';
 
-import {
-	defineCustomElements,
-	TdsHeader,
-	TdsHeaderBrandSymbol,
-	TdsHeaderHamburger,
-	TdsHeaderTitle,
-} from '@scania/tegel-react';
+import { useEffect, useState } from 'react';
+import { defineCustomElements, TdsHeader, TdsHeaderBrandSymbol, TdsHeaderHamburger, TdsHeaderTitle } from '@scania/tegel-react';
 import { Button } from './button';
 import { signOutAction } from '@/app/actions';
+import { createClient } from '@/utils/supabase/client';
 
 interface NavProps {
-	isLogged: boolean;
+  isLogged: boolean;
 }
 
 const Nav = ({ isLogged }: NavProps) => {
-  
-	defineCustomElements();
-	return (
-		<TdsHeader>
-			<TdsHeaderHamburger
-				onClick={() => {}}
-				aria-label="Open application drawer"
-				aria-haspopup="true"
-				aria-expanded="false"
-			></TdsHeaderHamburger>
+  const [session, setSession] = useState<any>(null); 
+  const supabase = createClient();
 
-			<TdsHeaderTitle>Github Dashboard</TdsHeaderTitle>
-			{isLogged && (
-				<form action={signOutAction}>
-					<Button type="submit" variant={'outline'}>
-						Sign out
-					</Button>
-				</form>
-			)}
-			<TdsHeaderBrandSymbol slot="end">
-				<a aria-label="Scania - red gryphon on blue shield"></a>
-			</TdsHeaderBrandSymbol>
-		</TdsHeader>
-	);
+  useEffect(() => {
+    const fetchSession = async () => {
+      const { data: { session } } = await supabase.auth.getSession();
+      setSession(session); 
+    };
+    fetchSession();
+  }, [supabase]);
+
+  defineCustomElements();
+
+  return (
+    <TdsHeader>
+      <TdsHeaderHamburger
+        onClick={() => {}}
+        aria-label="Open application drawer"
+        aria-haspopup="true"
+        aria-expanded="false"
+      />
+      <TdsHeaderTitle>Github Dashboard</TdsHeaderTitle>
+
+      {session?.user && (
+        <form action={signOutAction}>
+          <Button type="submit" variant={'outline'}>
+            Sign out
+          </Button>
+        </form>
+      )}
+      <TdsHeaderBrandSymbol slot="end">
+        <a aria-label="Scania - red gryphon on blue shield"></a>
+      </TdsHeaderBrandSymbol>
+    </TdsHeader>
+  );
 };
 
 export { Nav };
