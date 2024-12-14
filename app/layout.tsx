d1f52ac6d22463b195240da947155c0ce77731d5
helaasemy <hamzaelaasemy@gmail.com>
2024-12-14 14:29:41 +0100

    revert baseUrl

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
 					<main className="min-h-screen flex">
 						<div className="flex-1 w-full flex flex-col gap-16 ">
 							<Nav />
-							<div>{defaultUrl}</div>
 							<div className="flex flex-col p-5">
 								{children}
 							</div>
